32fd0fd1201eba5b7a20c53ca897f00fd170db22
Yosep <yosepalmanza@hotmail.com>
2022-04-14 11:41:24 -0500

    Fixed Issue With ImageAI

--- image.py
+++ image.py
@@ -8,6 +8,6 @@ prediction.setModelTypeAsResNet50()
 prediction.setModelPath(os.path.join(execution_path, "resnet50_imagenet_tf.2.0.h5"))
 prediction.loadModel()
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "sample.png"), result_count=5 )
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "sample1.png"), result_count=5 )
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
