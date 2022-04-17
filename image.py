35dc2c87d3aa1b24d243e2ef126ba8393669643b
Yosep <yosepalmanza@hotmail.com>
2022-04-17 14:23:20 -0500

    Refactored image.py
    Added models for AI algorithm.

--- image.py
+++ image.py
@@ -1,13 +1,46 @@
 from imageai.Classification import ImageClassification
+from heapq import heappop, heappush, heapify
 import os
 
-execution_path = os.getcwd()
 
-prediction = ImageClassification()
-prediction.setModelTypeAsResNet50()
-prediction.setModelPath(os.path.join(execution_path, "resnet50_imagenet_tf.2.0.h5"))
-prediction.loadModel()
+def getNormalPrediction():
+    execution_path = os.getcwd()
+    prediction = ImageClassification()
+    prediction.setModelTypeAsInceptionV3()
+    #prediction.setModelPath(os.path.join(execution_path, "\model\resnet50_imagenet_tf.2.0.h5"))
+    #prediction.setModelPath(execution_path+"\\model\\InceptionV3.h5")
+    print(execution_path +'\\model\\InceptionV3.h5')
+    prediction.setModelPath(execution_path +'\\model\\InceptionV3.h5')
+    prediction.loadModel()
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "sample1.png"), result_count=5 )
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction , " : " , eachProbability)
+    predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "sample1.png"), result_count=5 )
+    myPrediction = predictions[0] + " " + str(round(probabilities[0]))
+    return myPrediction
+
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
+
+def getFastPrediction():
+    execution_path = os.getcwd()
+    prediction = ImageClassification()
+    prediction.setModelTypeAsResNet50()
+    prediction.setModelPath(os.path.join(execution_path, "resnet50_imagenet_tf.2.0.h5"))
+    prediction.loadModel(prediction_speed="fastest")
+
+    predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "model/sample1.png"), result_count=5 )
+    myPrediction = predictions[0] + " " + str(round(probabilities[0])+"%")
+    return myPrediction
+
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
+
+def getPrediction(isPrecise):
+    if isPrecise == True:
+        print("Precision Prediction")
+        predict = getNormalPrediction()
+    else:
+        print("Fast Prediction")
+        predict = getFastPrediction()
+        print(predict)
+
+getPrediction(isPrecise=True)
